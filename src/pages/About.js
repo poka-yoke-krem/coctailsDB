359667d5059645402cbc670451e84ad53fe13979
ruslan <kremcheev.ruslan@gmail.com>
2022-10-01 16:10:28 +0300

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias eos sed
+        repellat aperiam consequuntur ab mollitia delectus nisi, consectetur
+        praesentium iure. Fuga nostrum ullam quos esse unde placeat quisquam
+        reprehenderit.
+      </p>
+    </section>
+  );
+};
+
+export default About;
